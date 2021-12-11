f6b63f5de1cbb64fc498b511de0794d5786fe081
Victor Costa <victorsc.job@gmail.com>
2021-12-11 20:35:31 -0300

    fix: Ajuste

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,67 @@
 import Vue from 'vue'
-import routes from './routes'
 import VueRouter from 'vue-router'
 import firebase from 'firebase/app'
+import db from '../plugins/firebse'
 
 Vue.use(VueRouter)
+const routes = [
+	{
+    path: '*',
+    redirect: '/signIn'
+  },
+  {
+    path: '/',
+    redirect: '/signIn'
+  },
+  {
+    path: '/signIn',
+    name: 'SignIn',
+    component: () => import(/* webpackChunkName: "signIn" */ '../modules/Authentication/SignIn/SignIn.vue')
+  },
+  {
+    path: '/signUp',
+    name: 'SignUp',
+    component: () => import(/* webpackChunkName: "signUp" */ '../modules/Authentication/SignUp/SignUp.vue')
+	},
+	{
+    path: '/manageusers',
+    name: 'ManageUsers',
+    component: () => import(/* webpackChunkName: "manageusers" */ '../modules/Users/ManageUsers/ManageUsers.vue'),
+    meta: {
+      requiresAuth: true,
+		},
+		beforeEnter: (to, from, next) => {
+			const uid = firebase.auth().currentUser.uid
+			db.collection('Users')
+				.doc(uid)
+				.get()
+				.then((res) => {
+					const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+					let user = res.data().role
+
+					if (!requiresAuth && user == 'Admin') next('manageusers')
+					else if (requiresAuth && user ==  'Default') next('todolist')
+					else next()
+				})
+		}
+	},
+	{
+    path: '/myaccount',
+    name: 'MyAccount',
+    component: () => import(/* webpackChunkName: "myaccount" */ '../modules/Users/MyAccount/MyAccount.vue'),
+    meta: {
+      requiresAuth: true,
+    },
+	},
+	{
+    path: '/todolist',
+    name: 'TodoList',
+    component: () => import(/* webpackChunkName: "todolist" */ '../views/TodoList/TodoList.vue'),
+    meta: {
+      requiresAuth: true,
+    },
+  },
+]
 
 const router = new VueRouter({
   mode: 'history',
--- src/router/routes.js
+++
@@ -1,9 +0,0 @@
-import { routes as views } from '@/views'
-import { routes as authentication } from '@/modules/Authentication'
-import { routes as users } from '@/modules/Users'
-
-export default [
-	... views,
-	... authentication,
-	... users
-]
